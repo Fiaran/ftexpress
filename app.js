b9b689a718c33465d7d86d5bfc85097cd64ea482
muratbek <muratbek@example.com>
2022-09-19 18:24:25 +0600

    Back

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 
@@ -16,15 +17,17 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views') // 2-views is package
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/',homeRoutes)
 app.use('/add',addRoutes)
 app.use('/courses',coursesRoutes)
+app.use('/card',cardRoutes)
 
 const PORT = process.env.PORT || 3000
-
+const password = 'admin'
+const url = 'mongodb+srv://admin:<admin>@cluster0.fen0xqs.mongodb.net/?retryWrites=true&w=majority'
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
